d94a650c053a71b48ffc75e1797a9f6d4e2df5e8
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-07-20 17:43:20 +0100

    :sparkles: Not Found component

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{ statusCode }}, sorry :)</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            statusCode: 404
+        }
+    }
+}
+</script>
