22098e87f315f487ea12827a58e744c4f15ecfaf
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-07-21 20:52:04 +0100

    🚧 Progress on organizations.

--- src/components/content_metadata/JobMetadata.vue
+++ src/components/content_metadata/JobMetadata.vue
@@ -29,13 +29,22 @@
             <template slot="header">
                 <h6 class="m-0">Organisation</h6>
             </template>
+
+            {{ thisOrganization }}
             
             <b-input v-model="organizationSearchQuery" placeholder="Type to search Organizations…"></b-input>
             <div class="mt-3">
                 <p v-show="organizationSearchQueryFetching || organizationSearchQueryDirty">Searching…</p>
-                <div v-if="organizationSearchResults.length">
-                    {{ organizationSearchResults }}
-                </div>
+                <b-list-group v-if="organizationSearchResults.length">
+                    <b-list-group-item
+                        v-for="org in flattenForest(organizationSearchResults)"
+                        :key="org.organization_id"
+                        :active="thisOrganization.id === org.organization_id"
+                        button
+                        @click="thisOrganization = org.path[org.path.length-1]">
+                        {{ org.path | formatPath }}
+                    </b-list-group-item>
+                </b-list-group>
                 <div v-else-if="organizationSearchQuery && !organizationSearchQueryDirty">
                     No organizations found.
                 </div>
@@ -62,6 +71,11 @@
 import _ from 'lodash';
 
 export default {
+    filters: {
+        formatPath(path) {
+            return path.map((e, i, a) => (i < a.length ? e.short_name : e.name)).join(' › ');
+        }
+    },
     props: {
         value: {
             type: Object,
@@ -117,25 +131,33 @@ export default {
             let queryArray = this.organizationSearchQuery.split(/[^a-zA-Z0-9]/).filter(w => w != '');
 
             this.$axios
-                .get(`/organizations?q=${queryArray.join(' ')}`)
-                .then(response => {
-                    this.organizationSearchQueryDirty = false;
-                    this.organizationSearchQueryFetching = false;
-                    
-                    this.organizationSearchResults = response.data;
-                })
-                .catch(error => {
-                    this.organizationSearchQueryDirty = false;
-                    this.organizationSearchQueryFetching = false;
+            .get(`/organizations?q=${queryArray.join(' ')}`)
+            .then(response => {
+                this.organizationSearchQueryDirty = false;
+                this.organizationSearchQueryFetching = false;
+                
+                this.organizationSearchResults = response.data;
+            })
+            .catch(_error => {
+                this.organizationSearchQueryDirty = false;
+                this.organizationSearchQueryFetching = false;
 
-                    this.organizationSearchResults = [];
-
-                    this.$root.$emit("global-notification", {
-                        type: "danger",
-                        message: `There was a problem retrieving the list of organizations.<br/>${error}`
-                    });
-                })
-        }, 500)
+                this.organizationSearchResults = [];
+                // TODO Signal a problem with search.
+            });
+        }, 500),
+        flattenTree(node, path = []) {
+            let _path = [...path, _.omit(node, 'children')];
+            if (node.children) {
+                return node.children.reduce((accumulator, child) => accumulator.concat(this.flattenTree(child, _path)), []);
+            }
+            return [_path];
+        },
+        flattenForest(forest) {
+            return forest.reduce((accumulator, node) => accumulator.concat(this.flattenTree(node)), [])
+                         .map(t => ({ organization_id: t[t.length-1].id, path: t }));
+        }
     }
 }
+
 </script>
