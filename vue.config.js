240a595d049d3e554212eb04639b4910bb1155fa
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-09-03 13:40:34 +0100

    Do things to the source mapping thing for Chrome

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     publicPath: process.env.VUE_APP_ROUTER_BASE || '/',
     configureWebpack: (config) => {
-        config.devtool = 'eval-source-map';
+        config.devtool = 'source-map';
+
+        config.output.devtoolModuleFilenameTemplate = info => info.resourcePath.match(/^\.\/\S*?\.vue$/)
+        ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+        : `webpack-yourCode:///${info.resourcePath}`;
+
+        config.output.devtoolFallbackModuleFilenameTemplate = 'webpack:///[resource-path]?[hash]';
     }
 };
