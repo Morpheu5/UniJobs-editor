86364976325419619b0dedc2f794235e0ea28aac
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-09-03 14:44:03 +0100

    :beers: Fix eslint and babel stuff.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
-    'vue/no-unused-vars': ['warning', { "argsIgnorePattern": "^_" }],
+    'vue/no-unused-vars': ['warn', { "argsIgnorePattern": "^_" }],
     'vue/html-indent': ['error', 4],
     'vue/attributes-order': 'error',
     'vue/order-in-components': 'error',
@@ -22,6 +22,7 @@ module.exports = {
     'camelcase': 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 6
   }
 };
